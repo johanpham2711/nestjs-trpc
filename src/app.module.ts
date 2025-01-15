8a1e6b2c07a34b9666307d81bd7f476c28250505
hungndSavvycom <hung.nguyen@savvycomsoftware.com>
2025-01-15 17:30:47 +0700

    feat: add trpc module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TrpcModule } from './modules';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TrpcModule],
 })
 export class AppModule {}
